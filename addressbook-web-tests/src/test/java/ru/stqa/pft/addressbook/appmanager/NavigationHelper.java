c89b3e405e47083eb20c3667b724cad82dda15d2
pteros76 <pterodactyl76@gmail.com>
2023-04-05 23:49:35 +0700

    Проверка необходимости перехода на страницу

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -5,17 +5,29 @@ import org.openqa.selenium.WebDriver;
 
 public class NavigationHelper extends HelperBase{
 
-    private WebDriver driver;
-    public NavigationHelper(WebDriver driver) {
+
+    public NavigationHelper(
+            WebDriver driver) {
        super(driver);
     }
 
     public void gotoGroupPage() {
+        if (
+                isElementPresent(By.tagName("h1"))
+            && driver.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))
+        )
+        {
+            return;
+        }
         click(By.linkText("groups"));
     }
 
     public void gotoNewContactPage(){click(By.linkText("add new"));}
 
-    public void gotoHomePage() {click(By.linkText("home"));
+    public void gotoHomePage() {
+        if(isElementPresent(By.id("maintable")))
+             return;
+        click(By.linkText("home"));
     }
 }
